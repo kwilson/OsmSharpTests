d2e71850f1dfd5f8435a4b50b7bed04279d3abae
Kev <kwilson@ibigroup.com>
2013-09-19 11:33:45 +0100

    Added status controller to check for engine running.

---
+++ OsmSharpTests/Controllers/StatusController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OsmSharpTests.Controllers
+{
+    using OsmSharpTests.MapEngine;
+
+    public class StatusController : ApiController
+    {
+        // GET api/status
+        public bool Get()
+        {
+            return Engine.IsReady;
+        }
+    }
+}
